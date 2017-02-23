e8d2dce96bf77485a8e86bab656027a15bb1ee0b
Aaron Zipursky <aaronzipursky@gmail.com>
2017-02-23 15:06:19 -0600

    forest shrinks as it depletes

--- Assets/Scripts/Resources/Forest.cs
+++ Assets/Scripts/Resources/Forest.cs
@@ -6,6 +6,9 @@ public class Forest : Resource {
 
 	private float elapsedTime = 0f;
 
+	private int originalSupply;
+	private Vector3 originalScale;
+
 	// Use this for initialization
 	void Start () {
 		associatedEnergyType = "wood";
@@ -16,6 +19,9 @@ public class Forest : Resource {
 		} else {
 			totalPotentialEnergy = potentialEnergyPerUnit * gameObject.transform.parent.transform.childCount;
 		}
+
+		originalSupply = totalPotentialEnergy;
+		originalScale = gameObject.transform.localScale;
 	}
 	
 	// Update is called once per frame
@@ -26,5 +32,7 @@ public class Forest : Resource {
 		} else {
 			elapsedTime += Time.deltaTime;
 		}
+		//make the forest look depleted
+		gameObject.transform.localScale = originalScale * ((totalPotentialEnergy / (float)(originalSupply*2)) + 0.5f);
 	}
 }
