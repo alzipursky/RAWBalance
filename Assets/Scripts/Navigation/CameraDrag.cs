d809451ed28a16c79f43f1333a88eeb65dcbceee
Aaron Zipursky <aaronzipursky@gmail.com>
2017-02-25 11:21:37 -0600

    clamped click and drag movement; still not attached to camera bc still buggy

--- Assets/Scripts/Navigation/CameraDrag.cs
+++ Assets/Scripts/Navigation/CameraDrag.cs
@@ -5,6 +5,12 @@ public class CameraDrag : MonoBehaviour
 	public float dragSpeed = 2;
 	private Vector3 dragOrigin;
 
+	private int yMinLimit = -10;
+	private int yMaxLimit = 100;
+
+	private int xMinLimit = -10;
+	private int xMaxLimit = 100;
+
 	void LateUpdate()
 	{
 		if (Input.GetMouseButtonDown(0))
@@ -16,9 +22,18 @@ public class CameraDrag : MonoBehaviour
 		if (!Input.GetMouseButton(0)) return;
 
 		Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-
 		Vector3 move = new Vector3(pos.x * dragSpeed, pos.y * dragSpeed,0);
-		transform.Translate(move, Space.World);
+		//if((transform. > xMinLimit && pos.x*dragSpeed<xMaxLimit) && (pos.y*dragSpeed > yMinLimit && pos.y*dragSpeed<yMaxLimit)){
+		//Debug.Log(transform.position);
+		//Debug.Log(move);
+
+		var worldMove = Camera.main.ViewportToWorldPoint(move);
+		var newX = transform.position.x + worldMove.x;
+		var newY = transform.position.y + worldMove.y;
+
+		if((newX > xMinLimit && newX<xMaxLimit) && (newY > yMinLimit && newY<yMaxLimit)){
+			transform.Translate(move, Space.World);
+		}
 	}
 
 
